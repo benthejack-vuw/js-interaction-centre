26b522fe93a2021925b395edb4bd50abacef2910
Ben <benthejack@gmail.com>
2017-03-01 18:30:38 +1300

    re-organizing output library to include module declaration

--- src/interaction_centre.ts
+++ src/interaction_centre.ts
@@ -1,11 +1,7 @@
-import {MouseData} from "./mouseData"
-import {MutableMouseData} from "./mouseData"
 import InteractionManager from "./interactionHandlers/interactionManager"
-import GuiInteractionHandler from "./interactionHandlers/guiInteractionHandler"
-import MouseInteractionHandler from "./interactionHandlers/mouseInteractionHandler"
-import KeyboardInteractionHandler from "./interactionHandlers/keyboardInteractionHandler"
 
 (<any>window).InteractionManager = InteractionManager;
-(<any>window).MouseInteractionManager = MouseInteractionHandler;
-(<any>window).KeyboardInteractionManager = KeyboardInteractionHandler;
-(<any>window).GuiInteractionManager = GuiInteractionHandler;
+
+declare module js_interaction_centre{
+	var InteractionCentre = InteractionManager;
+}
