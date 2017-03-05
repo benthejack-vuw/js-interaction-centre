912948ae53a0d79f3781af3fe37175f1119c3181
Ben Jack <ben@benjack>
2017-03-05 13:19:55 +1300

    lib reconfigured again

--- src/interactionHandlers/interactionManager.ts
+++ src/interactionHandlers/interactionManager.ts
@@ -10,17 +10,18 @@ import {MouseInteractionHandler} from "./mouseInteractionHandler"
 *	The interaction manager class deals with mouse,
 *	keyboard, and [eventually] touch interactions (not yet implemented)
 *
-*	It uses a json file to set up key and mouse combinations in the interaction_definitions_url`. 
+*	It uses a json file to set up key and mouse combinations in the interaction_definitions_url`.
 *
-*	The constructor takes a callback object whose methods are linked to by key 
-*	and mouse combinations in the interaction_definitions_url`. 
+*	The constructor takes a callback object whose methods are linked to by key
+*	and mouse combinations in the interaction_definitions_url`.
 * 	See example definitions file for how the links work.
 *
 *	all rights reserved Ben Jack
 */
+export {Transform}
 
 export class InteractionManager{
- 	
+
  	private _keyboardManager:KeyboardInteractionHandler;
 	private _mouseManager:MouseInteractionHandler;
 	private _guiManager:GUIInteractionHandler;
@@ -36,7 +37,7 @@ export class InteractionManager{
 		else
 			this.setupInteractions(interactionDefinitions);
 	}
-	
+
 	private setupInteractions = (JSON_data:any) => {
 		if(JSON_data.keyboard){
 			this._keyboardManager = new KeyboardInteractionHandler(this._callbackObject, this._domListenerElement, JSON_data.keyboard);
@@ -71,5 +72,3 @@ export class InteractionManager{
 	}
 
 }
-
-
